b718e4b1181d77cf805e4f451f0bbd70fdfaecd8
Daniel Nguyen <daniel@monograph.com>
2024-03-26 10:43:15 -0700

    tooltip

--- src/components/Collision/Collision.jsx
+++ src/components/Collision/Collision.jsx
@@ -1,18 +1,21 @@
 import style from "./collision.module.css";
-import * as HoverCard from '@radix-ui/react-hover-card';
+import * as HoverCard from "@radix-ui/react-hover-card";
 
 const Collision = ({ showCollision }) => {
   return (
     <div className={style.container}>
       <HoverCard.Root openDelay={0} closeDelay={0}>
         <HoverCard.Trigger>
-          <div className={showCollision ? style.textShow : style.text}>
-            Object Collision
-          </div>
+          {showCollision ? (
+            <div className={style.textShow}>Object Collision</div>
+          ) : (
+            <div style={{ height: "17px" }}></div>
+          )}
         </HoverCard.Trigger>
         <HoverCard.Portal>
           <HoverCard.Content className={style.content} side="top">
-          This is colliding with another object. Please adjust its position for a perfect fit.
+            This is colliding with another object. Please adjust its position
+            for a perfect fit.
           </HoverCard.Content>
         </HoverCard.Portal>
       </HoverCard.Root>
