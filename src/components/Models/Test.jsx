d5fc5139edd606cd45a47e746d048a494b0e2e74
Daniel Nguyen <daniel@monograph.com>
2024-05-03 14:24:38 +0900

    pause with models

--- src/components/Models/Test.jsx
+++ src/components/Models/Test.jsx
@@ -9,14 +9,14 @@ import * as THREE from "three";
 import { MeshStandardMaterial } from "three";
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
 
-export function Test(props) {
-  const { nodes } = useGLTF("/models/asdf.glb");
+export function Test({ color: color }) {
   const { nodes: nodes1 } = useGLTF("/models/wall.glb");
+  const { nodes: nodes2 } = useGLTF("/models/test.glb");
   const csg = useRef();
-  const material = new MeshStandardMaterial({ color: "lightblue" });
+  const material = new MeshStandardMaterial({ color: color });
+  console.log(nodes2)
 
-  const combinedGeometry = useMemo(() => {
-    // Extract geometries and merge them
+  const leftExtFace = useMemo(() => {
     const geometries = Object.values(nodes1).map(node => node.geometry.clone());
     return BufferGeometryUtils.mergeGeometries(geometries, false);
   }, [nodes1]);
@@ -24,22 +24,7 @@ export function Test(props) {
   return (
     <mesh receiveShadow castShadow>
       <Geometry ref={csg} useGroups>
-        <Base geometry={combinedGeometry} material={material} scale={10} position={[adjustForX, 0, adjustForY]} />
-        {/* {
-          Object.keys(nodes1).map((key) => {
-            return (
-              <Base
-                key={key}
-                scale={10}
-                position={[adjustForX, 0, adjustForY]}
-                geometry={nodes1[key].geometry}
-                material={material}
-              />
-            );
-          })
-        } */}
-        {/* <Addition geometry={nodes.mesh_1.geometry} material={material} scale={10} position={[adjustForX, 0, adjustForY]} />
-        <Addition geometry={nodes.mesh_2.geometry} material={material} scale={10} position={[adjustForX, 0, adjustForY]} /> */}
+        <Base geometry={nodes2.mesh_0.geometry} material={material} scale={10} position={[adjustForX, 0, adjustForY]} />
         <PivotControls
           lineWidth={3}
           anchor={[0, 0, 0]}
