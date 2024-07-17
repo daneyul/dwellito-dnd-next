22620035c01d23154f8d6c02d44b22dfc0a002bd
Daniel Nguyen <daniel@monograph.com>
2024-07-17 09:54:40 +0900

    favicon

--- src/app/[slug]/layout.js
+++ src/app/[slug]/layout.js
@@ -1,10 +1,5 @@
 import '../globals.css';
 
-export const metadata = {
-  title: 'Custom Homebuilder',
-  description: 'Dwellito',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,5 +1,14 @@
 import Content from '@/components/Content/Content';
 
+export async function generateMetadata() {
+  return {
+    title: `Configurator - Custom Cubes`,
+    icons: {
+      icon: '/favicon/custom-cubes.png',
+    },
+  };
+}
+
 export default async function Page({ params, searchParams }) {
   const slug = params.slug;
   const querySelectionData = searchParams.data;
