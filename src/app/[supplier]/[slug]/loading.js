77d153293e50f6ff53ce954ede403c6c5a72b630
Daniel Nguyen <dan.nguyen5@gmail.com>
2024-10-31 13:54:01 +0900

    loading

---
+++ src/app/[supplier]/[slug]/loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Loading() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div>
+                <p>Loading...</p>
+            </div>
+        </div>
+    );
+}
+
+export default Loading;
