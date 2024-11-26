74ed08272bbc3b66175e8c5f4edf366a7129f6f8
Daniel Nguyen <dan.nguyen5@gmail.com>
2024-11-26 22:17:43 +0900

    add mobilevisitor

--- src/components/MobileForm/Shed/MobileForm.jsx
+++ src/components/MobileForm/Shed/MobileForm.jsx
@@ -105,7 +105,7 @@ export const MobileForm = ({ supplier }) => {
       zipCode: zipCode,
       phoneNumber: data.phone,
       url: `https://custom.configure.so/${supplier}/${slug}/?data=${convertedSelections}`,
-      mobileVisitor: false,
+      mobileVisitor: true,
       currency: 'USD',
       priceTotal: orderTotal,
     };
