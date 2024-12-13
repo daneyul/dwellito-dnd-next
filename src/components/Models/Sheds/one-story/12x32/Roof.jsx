5e75b487b60bd3876279cae419f266fe02b92a94
Daniel Nguyen <dan.nguyen5@gmail.com>
2024-12-13 11:42:02 +0900

    gable roof

--- src/components/Models/Sheds/one-story/12x32/Roof.jsx
+++ src/components/Models/Sheds/one-story/12x32/Roof.jsx
@@ -9,7 +9,7 @@ const Roof = ({
   selectedShed,
   adjustForX,
   adjustForY,
-  selectedRoof
+  selectedRoof,
 }) => {
   const { nodes: slantNodes, materials: slantMaterials } = useGLTF(
     `/models/${supplier}/roofs/${selectedShedHeight}/${selectedShed.size}/slant.glb`
@@ -21,7 +21,7 @@ const Roof = ({
     `/models/${supplier}/roofs/${selectedShedHeight}/${selectedShed.size}/roof-battens.glb`
   );
   const clonedMaterial = gableMaterials.Roof_Exterior.clone();
-    clonedMaterial.map = exteriorPaint;
+  clonedMaterial.map = exteriorPaint;
   return (
     <group scale={8} dispose={null} position={[adjustForX, 0, adjustForY]}>
       <mesh
@@ -33,213 +33,114 @@ const Roof = ({
       />
       {selectedRoof.name === COMPONENT_NAMES.SLANT_ROOF ? (
         <>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={slantNodes.Trims.geometry}
-          material={slantMaterials.Vertical_Trim}
-          scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={slantNodes.Roof_Ceiling001.geometry}
-        material={slantMaterials.Roof_Ceiling}
-        scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={slantNodes.Roof_exterior001.geometry}
-        material={new THREE.MeshStandardMaterial({ map: exteriorPaint })}
-        scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={slantNodes.Roof_interior001.geometry}
-        material={slantMaterials.Roof_interior}
-        scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={slantNodes.Roof_Framing001.geometry}
-        material={slantMaterials.Framing_Wood}
-        scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={slantNodes.Roof_Siding001.geometry}
-        material={slantMaterials.Roof_Siding}
-        scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={slantNodes.Roof_Top001.geometry}
-        material={slantMaterials['galvanized  alum metal']}
-        scale={0.025}
-      />
-      </>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={slantNodes.Trims.geometry}
+            material={slantMaterials.Vertical_Trim}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={slantNodes.Roof_Ceiling001.geometry}
+            material={slantMaterials.Roof_Ceiling}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={slantNodes.Roof_exterior001.geometry}
+            material={new THREE.MeshStandardMaterial({ map: exteriorPaint })}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={slantNodes.Roof_interior001.geometry}
+            material={slantMaterials.Roof_interior}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={slantNodes.Roof_Framing001.geometry}
+            material={slantMaterials.Framing_Wood}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={slantNodes.Roof_Siding001.geometry}
+            material={slantMaterials.Roof_Siding}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={slantNodes.Roof_Top001.geometry}
+            material={slantMaterials['galvanized  alum metal']}
+            scale={0.025}
+          />
+        </>
       ) : (
         <>
-        <group scale={0.025}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_1.geometry}
-          material={gableMaterials.Vertical_Trim}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_2.geometry}
-          material={gableMaterials.Vertical_Trim}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_3.geometry}
-          material={gableMaterials.Vertical_Trim}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_4.geometry}
-          material={gableMaterials.Vertical_Trim}
-        />
-      </group>
-      <group scale={0.025}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_6.geometry}
-          material={gableMaterials.Roof_Siding}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_7.geometry}
-          material={gableMaterials.Roof_Siding}
-        />
-      </group>
-      <group scale={0.025}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_10.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_11.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_12.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_13.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_14.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_15.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_16.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_17.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_18.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_19.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_20.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_21.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_9.geometry}
-          material={gableMaterials.Framing_Wood}
-        />
-      </group>
-      <group scale={0.025}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_23.geometry}
-          material={gableMaterials.Roof_interior}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={gableNodes.Object_24.geometry}
-          material={gableMaterials.Roof_interior}
-        />
-      </group>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={gableNodes.Object_5.geometry}
-        material={gableMaterials['galvanized  alum metal']}
-        scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={gableNodes.Object_8.geometry}
-        material={gableMaterials.Roof_Ceiling}
-        scale={0.025}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={gableNodes.Object_22.geometry}
-        material={clonedMaterial}
-        scale={0.025}
-      />
+          <group scale={0.025}>
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={gableNodes.Object_2.geometry}
+              material={gableMaterials.Roof_Siding}
+            />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={gableNodes.Object_3.geometry}
+              material={gableMaterials.Roof_Siding}
+            />
+          </group>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={gableNodes.Object_1.geometry}
+            material={gableMaterials.Vertical_Trim}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={gableNodes.Object_4.geometry}
+            material={gableMaterials.Roof_Ceiling}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={gableNodes.Object_5.geometry}
+            material={gableMaterials.Framing_Wood}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={gableNodes.Object_6.geometry}
+            material={gableMaterials['galvanized  alum metal']}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={gableNodes.Roof_exteriorwall.geometry}
+            material={clonedMaterial}
+            scale={0.025}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={gableNodes.Object_8.geometry}
+            material={gableMaterials.Roof_interior}
+            scale={0.025}
+          />
         </>
       )}
     </group>
